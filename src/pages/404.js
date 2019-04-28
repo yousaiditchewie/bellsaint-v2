3d3ecaf5f3656ddeb5e3a68e58c191ee04a62790
Jacob Allen <jedijake@me.com>
2019-04-27 22:20:35 -0500

    Add link to home page on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from 'react';
+import Layout from '../components/Layout';
+import { Link } from 'gatsby';
 
 const NotFoundPage = () => (
   <Layout>
     <div>
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Link to="/">Back to safety</Link>
     </div>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
