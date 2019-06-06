b54a89a4a7d9663e31e6bb9ca109c0fcf7edea21
Jacob Allen <jedijake@me.com>
2019-06-05 20:08:26 -0500

    Add preview template to cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,13 @@
 import CMS from 'netlify-cms';
 
 import LearnPagePreview from './preview-templates/LearnPagePreview';
+import ListenPagePreview from './preview-templates/ListenPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
 import ProductPagePreview from './preview-templates/ProductPagePreview';
 import IndexPagePreview from './preview-templates/IndexPagePreview';
 
 CMS.registerPreviewTemplate('index', IndexPagePreview);
 CMS.registerPreviewTemplate('learn', LearnPagePreview);
+CMS.registerPreviewTemplate('listen', ListenPagePreview);
 CMS.registerPreviewTemplate('products', ProductPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
