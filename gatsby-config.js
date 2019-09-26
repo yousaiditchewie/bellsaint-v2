47dd09932e89cd291855e81de065ad4a10657cac
Jacob Allen <jedijake@me.com>
2019-09-25 22:18:46 -0500

    yarn add gatsby-plugin-mailchimp

--- gatsby-config.js
+++ gatsby-config.js
@@ -106,6 +106,12 @@ module.exports = {
         purgeOnly: ['/all.sass'] // applies purging only on the bulma css file
       }
     }, // must be after other CSS plugins
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+        endpoint: '/' // @todo - needs actual Mailchimp URL
+      }
+    },
     'gatsby-plugin-netlify' // make sure to keep it last in the array
   ],
   // for avoiding CORS while developing Netlify Functions locally
