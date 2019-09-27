45c4740dbe50711b2adc69b9c07d451993d0de25
Jacob Allen <jedijake@me.com>
2019-09-26 20:54:35 -0500

    Add Signup form without yarn updates

--- src/components/footer/Signup.js
+++ src/components/footer/Signup.js
@@ -1,9 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import addToMailchimp from 'gatsby-plugin-mailchimp';
 
-const Footer = class extends React.Component {
-  render() {
-    return <div className="Signup">Signup Form</div>;
+const Signup = () => {
+  // Since `addToMailchimp` returns a promise, you
+  // can handle the response in two different ways:
+
+  // Note that you need to send an email & optionally, listFields
+  // these values can be pulled from React state, form fields,
+  // or wherever.  (Personally, I recommend storing in state).
+
+  const [email, setEmail] = useState('');
+  const [strangerDanger, setStrangerDanger] = useState(false);
+  const [mailchimpResponse, setMailchimpResponse] = useState({});
+
+  function handleChange(e) {
+    if (e.target.name === 'birthday') {
+      setStrangerDanger(true);
+      return;
+    }
+    setEmail(e.target.value);
   }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (strangerDanger) return;
+
+    addToMailchimp(email)
+      .then(responseData => {
+        /*
+         * responseData will take this shape:
+         * {
+         *  result: String, "success" or "error"
+         *  msg: String
+         * }
+         */
+        setMailchimpResponse(responseData);
+      })
+      .catch(responseError => {
+        setMailchimpResponse(responseError);
+      });
+  }
+
+  return (
+    <div className="Signup">
+      <div className="Signup-container container">
+        <form onSubmit={handleSubmit}>
+          <input
+            className="Signup-email"
+            type="text"
+            onChange={handleChange}
+            placeholder="email"
+            name="email"
+          />
+          <input
+            className="birthday"
+            type="text"
+            onChange={handleChange}
+            name="birthday"
+          />
+          <input
+            className="Signup-submit button"
+            type="submit"
+            value="submit"
+          />
+        </form>
+        {mailchimpResponse.result === 'success' ? (
+          <p
+            className="Signup-success"
+            dangerouslySetInnerHTML={{ __html: mailchimpResponse.msg }}
+          />
+        ) : (
+          ''
+        )}
+        {mailchimpResponse.result === 'error' ? (
+          <p
+            className="Signup-error"
+            dangerouslySetInnerHTML={{ __html: mailchimpResponse.msg }}
+          />
+        ) : (
+          ''
+        )}
+      </div>
+    </div>
+  );
 };
 
-export default Footer;
+export default Signup;
