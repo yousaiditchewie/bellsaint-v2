452a18887f491f1c14d3377168e7d866a0ae46c7
Jacob Allen <jedijake@me.com>
2019-09-30 20:49:46 -0500

    Try another way

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -4,19 +4,19 @@ import { HTMLContent } from '../../components/Content';
 import { IndexPageTemplate } from '../../templates/index-page';
 
 const IndexPagePreview = ({ entry, widgetFor }) => {
-  const data = entry.getIn(['data']).toJS();
+  // const data = entry.getIn(['data']).toJS();
 
-  if (data) {
-    return (
-      <IndexPageTemplate
-        contentComponent={HTMLContent}
-        image={data.image}
-        content={widgetFor('body')}
-      />
-    );
-  } else {
-    return <div>Loading...</div>;
-  }
+  // if (data) {
+  return (
+    <IndexPageTemplate
+      contentComponent={HTMLContent}
+      image={entry.getIn(['data', 'image'])}
+      content={widgetFor('body')}
+    />
+  );
+  // } else {
+  // return <div>Loading...</div>;
+  // }
 };
 
 IndexPagePreview.propTypes = {
